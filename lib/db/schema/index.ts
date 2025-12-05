820e0c5d995fa59423b335ea87c1f662aa30bf3b
erickrneves <137239004+erickrneves@users.noreply.github.com>
2025-12-05 17:16:46 -0300

    fix: resolve conflito de exports duplicados em schema index

--- lib/db/schema/index.ts
+++ lib/db/schema/index.ts
@@ -3,9 +3,23 @@ export * from './organizations'
 export * from './documents'
 export * from './sped'
 export * from './rag'
-export * from './document-schemas'
-export * from './normalization-templates'
 export * from './normalized-data'
 export * from './normalized-data-items'
 export * from './ecd-results'
 export * from './ecd-plano-referencial'
+
+// Export document-schemas
+export {
+  documentSchemas,
+  documentSchemasRelations,
+  insertDocumentSchemaSchema,
+  selectDocumentSchemaSchema,
+} from './document-schemas'
+
+// Export normalization-templates
+export {
+  normalizationTemplates,
+  normalizationTemplatesRelations,
+  insertNormalizationTemplateSchema,
+  selectNormalizationTemplateSchema,
+} from './normalization-templates'
